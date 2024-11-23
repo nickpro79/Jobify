aa316354f234270d12f922ec9fd8c2b762185cec
Nikhil <nikhilpraveen7777@gmail.com>
2024-11-23 13:04:13 +0530

    create create-jobs route

--- server.js
+++ server.js
@@ -31,6 +31,17 @@ app.get("/api/v1/jobs", (req, res) => {
   res.status(200).json({ jobs });
 });
 
+app.post("/api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "Please provide company and position" });
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(200).json({ job });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
