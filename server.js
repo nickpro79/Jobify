19af81fa82586d542c6b3c784b90f8fa8e70da30
Nikhil <nikhilpraveen7777@gmail.com>
2024-11-23 12:42:09 +0530

    create get-all-jobs route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import morgan from "morgan";
+import { nanoid } from "nanoid";
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
@@ -9,7 +10,13 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const port = process.env.PORT || 5100;
-if (process.env) app.use(express.json());
+
+let jobs = [
+  { id: nanoid(), company: "apple", position: "frontend" },
+  { id: nanoid(), company: "google", position: "backend" },
+];
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -18,6 +25,10 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data recieved", data: req.body });
+});
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 app.listen(port, () => {
